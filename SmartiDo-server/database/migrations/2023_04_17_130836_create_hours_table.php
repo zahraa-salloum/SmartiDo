01baa2f470c5a174f82d6ae4e44ae60a0ac0c4d0
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-17 23:17:28 +0300

    Updated hours migration

--- SmartiDo-server/database/migrations/2023_04_17_130836_create_hours_table.php
+++ SmartiDo-server/database/migrations/2023_04_17_130836_create_hours_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('hours', function (Blueprint $table) {
             $table->id();
             $table->integer("hours_count");
+            $table->string("category");
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id') -> references('id') -> on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->unsignedBigInteger('plan_id');
