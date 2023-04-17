5e7b7b84ffe3f407147bd5c7373e001c6550c915
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-17 15:58:38 +0300

    Created the exams migration

---
+++ SmartiDo-server/database/migrations/2023_04_17_125249_create_exams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("category");
+            $table->integer("hours_of_study");
+            $table->date("day");
+            $table->string("hour");
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id') -> references('id') -> on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exams');
+    }
+};
