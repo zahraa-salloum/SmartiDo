003489fecf9b9b92014ea74b486a75a2a93c754e
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-17 15:43:22 +0300

    Created the types migration

--- SmartiDo-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ SmartiDo-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id') -> references('id') -> on('types')->onUpdate('cascade')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
