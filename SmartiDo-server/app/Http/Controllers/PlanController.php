f3a8c7de8fe796c95d84d812e1d8b5fad6d73bea
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-20 01:49:52 +0300

    Created generate plan function and specified the variables from request

--- SmartiDo-server/app/Http/Controllers/PlanController.php
+++ SmartiDo-server/app/Http/Controllers/PlanController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PlanController extends Controller
 {
-    //
+    function generatePlan(Request $request) {
+        $id = Auth::id();
+        $sleep = $request->sleep;
+        $wake_up = $request->wake_up;
+        $breakfast = $request->breakfast;
+        $lunch = $request->lunch;
+        $dinner = $request->dinner;
+        $title = $request->title;
+        $category = $request->category;
+        $hours_of_study = $request->hours_of_study;
+        $day = $request->day;
+        $hour = $request->hour;
+
+    }
 }
