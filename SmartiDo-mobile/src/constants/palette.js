f671581df4cb92becaa1a15aaf3892fecd99a7b0
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 03:10:38 +0300

    Created palette file for constant colors

---
+++ SmartiDo-mobile/src/constants/palette.js
@@ -0,0 +1,29 @@
+export const colors = {
+    dark_purple: '#420F38',
+    purple: '#974E8A',
+    light_purple: '#E4C0DD',
+    black: '#000000',
+    white: '#FFFFFF',
+}
+
+export const shadows = {
+    sm: {
+        color: '#606470',
+        opacity: 0.15,
+        x: 0,
+        y: 3,
+        blur: 4
+    },
+    md: {
+        color: '#606470',
+        opacity: 0.15,
+        x: 0,
+        y: 5,
+        blur: 20
+    }
+}
+
+export default {
+    colors,
+    shadows
+}
