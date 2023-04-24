a94fe2c9f6535dced536ed3ac882fc972650455e
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 17:23:07 +0300

    Changed sizes of text in empty state

--- SmartiDo-mobile/src/components/EmptyState/styles.ts
+++ SmartiDo-mobile/src/components/EmptyState/styles.ts
@@ -5,7 +5,7 @@ export default StyleSheet.create({
     title:{
         color: colors.purple, 
         padding: 10,
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: 'bold',
         textAlign: 'center',
         marginTop: 5,
@@ -14,7 +14,7 @@ export default StyleSheet.create({
     description: {
         color: colors.dark_purple,  
         padding: 1,
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: 'bold',
         textAlign: 'center',
         },
