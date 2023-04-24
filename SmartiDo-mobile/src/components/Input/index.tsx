fd3791a3969601f17fc541770ce8d443e67cde24
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 13:06:26 +0300

    Created input component

---
+++ SmartiDo-mobile/src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import { Text, View, ViewStyle, TextStyle } from 'react-native';
+import styles from './styles';
+import { TextInput } from 'react-native';
+
+interface LabelledTextProps {
+  label: string;
+  placeholder: string;
+}
+
+const LabelledText: FC<LabelledTextProps> = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}> {props.label} </Text>
+      <TextInput style={styles.input} placeholder= {props.placeholder}></TextInput>
+    </View>
+  );
+};
+
+export default LabelledText;
