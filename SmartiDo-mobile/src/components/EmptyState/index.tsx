7d3546043bd50f4d31881846033c849b25d4fd6d
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 14:17:18 +0300

    Created empty state component

---
+++ SmartiDo-mobile/src/components/EmptyState/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import React, { FC } from 'react'
+
+interface EmptyStateProps  {
+
+}
+
+const EmptyState: FC<EmptyStateProps> = (props) => {
+return (
+    <View>
+    </View>
+  )
+}
+
+export default EmptyState
