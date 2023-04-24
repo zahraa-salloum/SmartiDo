ee31f49cc966b172c96d4bf32f8f0c626a0b556b
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 14:26:16 +0300

    Added title and description to empty state component

--- SmartiDo-mobile/src/components/EmptyState/index.tsx
+++ SmartiDo-mobile/src/components/EmptyState/index.tsx
@@ -2,12 +2,15 @@ import { Text, View } from 'react-native'
 import React, { FC } from 'react'
 
 interface EmptyStateProps  {
-
+    title?: string,
+    description?: string,
 }
 
 const EmptyState: FC<EmptyStateProps> = (props) => {
 return (
     <View>
+        {!!props.title ? <Text>{props.title}</Text> : null}
+        {!!props.description ? <Text>{props.description}</Text> : null}
     </View>
   )
 }
