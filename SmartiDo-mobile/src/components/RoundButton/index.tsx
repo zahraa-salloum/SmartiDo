19e1054ab66846c67f297a18767b8658b13ffc7f
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 15:20:49 +0300

    Created Round button component

---
+++ SmartiDo-mobile/src/components/RoundButton/index.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps, Text, TouchableOpacity, View } from 'react-native'
+import React, { FC } from 'react'
+
+
+interface RoundButtonProps  {
+  buttonprops: ButtonProps
+}
+
+const RoundButton: FC<RoundButtonProps> = (props) => {
+  return (
+    <>
+    </>
+)
+}
+
+export default RoundButton
