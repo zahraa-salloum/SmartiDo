fcd68d7566ca7194b281127fc188f2a45d2daa1e
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-24 15:09:32 +0300

    Added empty state for plans screen

--- SmartiDo-mobile/src/screens/plans/index.tsx
+++ SmartiDo-mobile/src/screens/plans/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import styles from './styles'
 import { ImageBackground, SafeAreaView } from 'react-native'
+import EmptyState from '../../components/EmptyState'
 
 interface PlansScreenProps  {}
 
@@ -8,6 +9,7 @@ const PlansScreen: FC<PlansScreenProps> = (props) => {
 return (
     <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
         <SafeAreaView style={styles.container}>
+            <EmptyState image={require('../../../assets/plans.png')} title='No Plan Yet' description='Tap + to create' />
         </SafeAreaView>
     </ImageBackground>
 )
