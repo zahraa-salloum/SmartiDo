66e1c0ceb1735464447a14c1231a042b244ae169
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-25 12:07:08 +0300

    Frontend: Created the text button component

---
+++ SmartiDo-mobile/src/components/TextButton/index.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps, Text, TouchableOpacity, View } from 'react-native'
+import React, { FC } from 'react'
+
+
+interface TextButtonProps  {
+  buttonprops: ButtonProps
+}
+
+const TextButton: FC<TextButtonProps> = (props) => {
+return (
+    <>
+</>
+)
+}
+
+export default TextButton
