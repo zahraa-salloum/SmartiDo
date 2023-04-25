1ae44dd84e4bc079837530309630faf2c152c593
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-25 12:10:52 +0300

    Frontend: Added touchable opacity the text button component

--- SmartiDo-mobile/src/components/TextButton/index.tsx
+++ SmartiDo-mobile/src/components/TextButton/index.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonProps, Text, TouchableOpacity, View } from 'react-native'
 import React, { FC } from 'react'
-
+import styles from './styles'
 
 interface TextButtonProps  {
   buttonprops: ButtonProps
@@ -9,6 +9,9 @@ interface TextButtonProps  {
 const TextButton: FC<TextButtonProps> = (props) => {
 return (
     <>
+        <TouchableOpacity  onPress={props.buttonprops.onPress} style={styles.button}>
+            <Text style={styles.text}> {props.buttonprops.title} </Text>
+        </TouchableOpacity>
     </>
 )
 }
