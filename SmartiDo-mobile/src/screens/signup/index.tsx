5a57f1dd36436a04cdde28d5d5c72942bf3283ce
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-25 17:49:52 +0300

    Frontend: Implemented register api

--- SmartiDo-mobile/src/screens/signup/index.tsx
+++ SmartiDo-mobile/src/screens/signup/index.tsx
@@ -7,6 +7,7 @@ import LabelledText from '../../components/LabelledText'
 import SeventyWidthButton from '../../components/SeventyWidthButton'
 import TextButton from '../../components/TextButton'
 import { login } from '../../redux/slices/authSlice'
+import axios from "axios"
 
 interface SignupScreenProps  {}
 
@@ -17,6 +18,7 @@ const SignupScreen: FC<SignupScreenProps> = (props) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error,setError]=useState("");
 
     const handleName=(text: React.SetStateAction<string>)=>{
         setName(text)
@@ -37,6 +39,37 @@ const SignupScreen: FC<SignupScreenProps> = (props) => {
         return passwordRegex.test(password);
     }
 
+    const handleSubmit=()=>{
+        if (name==="" || email==="" || password===""){
+            setError("All input are required")
+        }else{
+            if (validateEmail(email)){
+                if(validatePassword(password)){
+                    
+                    let data = {
+                        "name": name,
+                        "email": email,
+                        "password": password
+                      };
+    
+                    axios.post("http://192.168.1.105:8000/api/v0.0.1/register",data).then((res) => {
+                        if(res.data.status == "success"){
+                            dispatch(login());
+                            console.log(res.data)
+                        }
+                        
+                    }).catch((err) => {
+                        console.log(err);
+                    })
+                }else{
+            setError("Password must contain 8 chracters, one capital letter, one special character")
+                }
+            }else{
+            setError("Invalid email format")
+            }
+        }
+    }
+
 return (
     <ImageBackground source={require('../../../assets/signup.png')} style={styles.containerBackground}>
         <SafeAreaView style={styles.container}>
@@ -46,12 +79,8 @@ return (
             <LabelledText label='Password' placeholder='******' onChangeText={handlePassword}/>
             <SeventyWidthButton buttonprops={{
             title: "SIGN UP",
-            onPress: () => {
-                // api login
-                // wait for a respponse
-                // 200 => dispatch(login())
-                dispatch(login())
-              }
+            onPress: handleSubmit,
+            
             }} />
             <TextButton buttonprops={{
             title: "Already a User?",
