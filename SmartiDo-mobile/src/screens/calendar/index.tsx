c2f819baffd1d179f1d40a292fd7955004edbe3a
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-26 19:59:35 +0300

    Frontend: Added background image and safe area view for calendar

--- SmartiDo-mobile/src/screens/calendar/index.tsx
+++ SmartiDo-mobile/src/screens/calendar/index.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react'
+import { ImageBackground, SafeAreaView } from 'react-native'
 import styles from './styles'
 
 interface CalendarScreenProps  {}
 
 const CalendarScreen: FC<CalendarScreenProps> = (props) => {
     return (
+        <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
+            <SafeAreaView style={styles.container}>
+            </SafeAreaView>
+        </ImageBackground>
     )
 }
 
