396324c70bfc593317fc25e84f012c21a84f796b
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-26 19:46:02 +0300

    Frontend: Created leaderboard screen

---
+++ SmartiDo-mobile/src/screens/leaderboard/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+
+interface CalendarScreenProps  {}
+
+const CalendarScreen: FC<CalendarScreenProps> = (props) => {
+    return (
+    )
+}
+
+export default CalendarScreen
