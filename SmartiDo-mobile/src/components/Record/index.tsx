3cf0b66551c94ab4b0af99803c025f39e48b80d9
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-26 21:36:20 +0300

    Frontend: Created record component

---
+++ SmartiDo-mobile/src/components/Record/index.tsx
@@ -0,0 +1,21 @@
+import { Image, ImageSourcePropType, Text, View } from 'react-native'
+import React, { FC } from 'react'
+import styles from './styles'
+
+interface RecordProps  {
+    image?: ImageSourcePropType,
+    name?: string,
+    score?: number,
+}
+
+const Record: FC<RecordProps> = (props) => {
+return (
+    <View>
+        {!!props.image ? <Image source={props.image}></Image> : null}
+        {!!props.name ? <Text style={styles.name}>{props.name}</Text> : null}
+        {!!props.score ? <Text style={styles.score}>{props.score}</Text> : null}
+    </View>
+  )
+}
+
+export default Record
