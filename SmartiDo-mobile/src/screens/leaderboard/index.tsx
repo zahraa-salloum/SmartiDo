bdbde888011a33516ed380fbd6f67e6ee046f352
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-26 19:58:49 +0300

    Frontend: Added background image and safe area view for leaderboard

--- SmartiDo-mobile/src/screens/leaderboard/index.tsx
+++ SmartiDo-mobile/src/screens/leaderboard/index.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react'
+import { ImageBackground, SafeAreaView } from 'react-native'
 import styles from './styles'
 
 interface LeaderboardScreenProps  {}
 
 const LeaderboardScreen: FC<LeaderboardScreenProps> = (props) => {
     return (
+        <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
+            <SafeAreaView style={styles.container}>
+            </SafeAreaView>
+        </ImageBackground>
     )
 }
 
