c60650fc9b66ff47118442e38d01cd4db79fc27e
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-26 19:54:42 +0300

    Frontend: Added style file to calendar

---
+++ SmartiDo-mobile/src/screens/calendar/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+import { colors } from '../../constants/palette'
+
+export default StyleSheet.create({
+    containerBackground: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      width: "90%",
+    }
+})
