bea255a4a6a2f6b353f8ddf4bb41e83fdd226ba0
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-28 14:33:19 +0300

    Frontend: Created task component

---
+++ SmartiDo-mobile/src/components/Task/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native'
+import React, { FC } from 'react'
+
+
+interface TaskProps  {
+}
+
+const Task: FC<TaskProps> = (props) => {
+    return (
+        <View>
+        
+        </View>
+    ) 
+}
+
+export default Task
