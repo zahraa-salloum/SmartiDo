98cc76055568714063089cc5df4ec209492ead1a
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-28 20:05:38 +0300

    Frontend: Implemented add todo api by creating dialog input

--- SmartiDo-mobile/src/screens/tasks/index.tsx
+++ SmartiDo-mobile/src/screens/tasks/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react'
-import { ImageBackground, SafeAreaView, ScrollView } from 'react-native'
+import { Alert, ImageBackground, SafeAreaView, ScrollView } from 'react-native'
 import styles from './styles'
 import EmptyState from '../../components/EmptyState'
 import RoundButton from '../../components/RoundButton'
@@ -7,11 +7,13 @@ import Task from '../../components/Task'
 import axios from 'axios'
 import { numbers } from '../../constants/constants'
 import * as SecureStore from 'expo-secure-store'
+import DialogInput from 'react-native-dialog-input'
 
 interface TasksScreenProps  {}
 
 const TasksScreen: FC<TasksScreenProps> = (props) => {
     const [todos, setTodos] = useState([]);
+    const [visible, setVisible] = React.useState(false);
 
     useEffect(() => {
         
@@ -28,7 +30,7 @@ const TasksScreen: FC<TasksScreenProps> = (props) => {
             fetchTodos();
       }, []);
 
-      const handleDone = async (todo: { id: any }) => {
+    const handleDone = async (todo: { id: any }) => {
         const token = await SecureStore.getItemAsync('token');
       
         const data = {
@@ -47,7 +49,40 @@ const TasksScreen: FC<TasksScreenProps> = (props) => {
           .catch((error) => {
             console.log(error);
           });
-      };
+    };
+
+    const handleAddTask = async (task: string) => {
+        const token = await SecureStore.getItemAsync('token');
+        
+        const data = {
+          to_do: task,
+        };
+        
+        axios.post('http://' + numbers.server + '/api/v0.0.1/add_todo', data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(response => {
+          setTodos(prevTodos => [...prevTodos, response.data.todo]);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+
+    const handleAddTaskPress = () => {
+        setVisible(true);
+    };
+    
+    const handleSubmitTask = (task: string) => {
+        handleAddTask(task);
+        setVisible(false);
+    };
+    
+    const handleCloseDialog = () => {
+        setVisible(false);
+    };
 
 
     const renderTasks = () => {
@@ -60,25 +95,35 @@ const TasksScreen: FC<TasksScreenProps> = (props) => {
             />
           );
         } else {
-          return (
-            <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent}>
-              {todos.map((todo) => (
-                <Task key={todo.id} text={todo.to_do} onPress={() => handleDone(todo)} />
-              ))}
-            </ScrollView>
-          );
+            return (
+                <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent}>
+                    {todos.map((todo) => (
+                        <Task key={todo.id} text={todo.to_do} onPress={() => handleDone(todo)} />
+                    ))}
+                </ScrollView>
+            );
         }
-      };
-return (
-    <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
-        <SafeAreaView style={styles.container}>
-            {renderTasks()}
-            <RoundButton buttonprops={{
-            title: "+",
-            }} />
-        </SafeAreaView>
-    </ImageBackground>
-)
+    };
+
+    return (
+        <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
+            <SafeAreaView style={styles.container}>
+                {renderTasks()}
+                <RoundButton buttonprops={{
+                title: "+",
+                onPress: handleAddTaskPress,
+                }} />
+                <DialogInput
+                isDialogVisible={visible}
+                title={'Task'}
+                message={'Enter Task'}
+                hintInput={'Enter Text'}
+                submitInput={handleSubmitTask}
+                closeDialog={handleCloseDialog}
+            />
+            </SafeAreaView>
+        </ImageBackground>
+    )
 }
 
 export default TasksScreen
