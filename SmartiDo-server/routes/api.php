0d5e3836ac15c529daa41c658ecf5c6f0902ec2b
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 10:37:42 +0300

    Backend: Readability fix

--- SmartiDo-server/routes/api.php
+++ SmartiDo-server/routes/api.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::group(['prefix' => 'v0.0.1'], function(){
-    Route::group(['prefix' => 'auth'], function () {
+    Route::group(['prefix' => 'auth'], function (){
         Route::post('login',[AuthController::class, 'login']);  
         Route::post('logout',[AuthController::class, 'logout']);
         Route::post('refresh',[AuthController::class, 'refresh']);
