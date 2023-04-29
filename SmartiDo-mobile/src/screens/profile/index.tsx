ffb257cc9013b684a6a9bf56c786b4672279e4f7
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 15:08:01 +0300

    Frontend: Created profile screen

---
+++ SmartiDo-mobile/src/screens/profile/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC} from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+
+interface ProfileScreenProps  {}
+
+const ProfileScreen: FC<ProfileScreenProps> = (props) => {
+    return (
+    )
+}
+
+export default ProfileScreen
