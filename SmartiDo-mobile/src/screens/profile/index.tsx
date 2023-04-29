562874d02bfa069b49492afcdcd02f62e94f157e
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 15:14:55 +0300

    Frontend: Added image background for profile screen

--- SmartiDo-mobile/src/screens/profile/index.tsx
+++ SmartiDo-mobile/src/screens/profile/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
+import { ImageBackground } from 'react-native'
 
 
 interface ProfileScreenProps  {}
 
 const ProfileScreen: FC<ProfileScreenProps> = (props) => {
     return (
+        <ImageBackground source={require('../../../assets/signup.png')} style={styles.containerBackground}>
+        </ImageBackground>
     )
 }
 
