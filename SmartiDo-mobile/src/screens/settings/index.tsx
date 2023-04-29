665dbb429b582e070adfff2832561b31a096be31
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 10:16:41 +0300

    Frontend: Added dialog for delete plan

--- SmartiDo-mobile/src/screens/settings/index.tsx
+++ SmartiDo-mobile/src/screens/settings/index.tsx
@@ -1,11 +1,27 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { ImageBackground, SafeAreaView } from 'react-native'
 import styles from './styles'
 import SettingsButton from '../../components/SettingsButton'
+import Dialog from "react-native-dialog";
 
 interface SettingsScreenProps  {}
 
 const SettingsScreen: FC<SettingsScreenProps> = (props) => {
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => {
+        setVisible(true);
+      };
+    
+    const handleCancel = () => {
+        setVisible(false);
+    };
+    
+    const handleDelete = () => {
+        
+        setVisible(false);
+    };
+    
     return (
         <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
             <SafeAreaView style={styles.container}>
@@ -15,8 +31,16 @@ const SettingsScreen: FC<SettingsScreenProps> = (props) => {
                 }}/>
                 <SettingsButton  buttonprops={{
                 title: "Delete Plan",
-                
+                onPress: showDialog,
                 }}/>
+                <Dialog.Container visible={visible}>
+                    <Dialog.Title>Plan Delete</Dialog.Title>
+                    <Dialog.Description>
+                        Do you want to delete this plan? You cannot undo this action.
+                    </Dialog.Description>
+                    <Dialog.Button label="Cancel" onPress={handleCancel} />
+                    <Dialog.Button label="Delete" onPress={handleDelete} />
+                </Dialog.Container>
                 <SettingsButton  buttonprops={{
                 title: "Logout",
                 
