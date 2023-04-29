b16f03b164cd3abd2e2f11a74d02f964023fa2a5
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 18:58:58 +0300

    Frontend: Implemented save profile (picture)

--- SmartiDo-mobile/src/screens/profile/index.tsx
+++ SmartiDo-mobile/src/screens/profile/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC, useState} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
-import { Button, ImageBackground, SafeAreaView } from 'react-native'
+import { Button, ImageBackground, SafeAreaView, ToastAndroid } from 'react-native'
 import SeventyWidthButton from '../../components/SeventyWidthButton'
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import TextButton from '../../components/TextButton'
 import ImageProfile from '../../components/ImageProfile'
+import axios from 'axios'
+import { numbers } from '../../constants/constants'
+import * as SecureStore from 'expo-secure-store'
 
 interface ProfileScreenProps  {}
 
@@ -27,6 +30,29 @@ const ProfileScreen: FC<ProfileScreenProps> = (props) => {
         }
       };
 
+    const handleSave = async () => {
+        const token = await SecureStore.getItemAsync('token');
+        // console.log(base64String)
+        const data = {
+            picture: base64String,
+            bio: "Hi",
+            gender: "female",
+            dob: "1996-10-30",
+        };
+        
+        axios.post('http://' + numbers.server + '/api/v0.0.1/add_profile', data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(response => {
+            ToastAndroid.show("Saved", ToastAndroid.SHORT);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+
     return (
         <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
             <SafeAreaView style={styles.container}>
@@ -37,7 +63,7 @@ const ProfileScreen: FC<ProfileScreenProps> = (props) => {
                 }}/>
                 <SeventyWidthButton buttonprops={{
                 title: "Save",
-                
+                onPress: handleSave,
                 }} />
             </SafeAreaView>
         </ImageBackground>
