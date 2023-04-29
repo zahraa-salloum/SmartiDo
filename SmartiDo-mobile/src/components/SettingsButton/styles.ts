2b84dd7bec5f9d4ec0cd99caf9cdd0f572f4f3d1
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 10:04:36 +0300

    Frontend: Increased height of button in settings button component

--- SmartiDo-mobile/src/components/SettingsButton/styles.ts
+++ SmartiDo-mobile/src/components/SettingsButton/styles.ts
@@ -4,12 +4,12 @@ import { colors } from '../../constants/constants'
 export default StyleSheet.create({
     button:{
         flexDirection: 'row',
-        marginLeft: 20,
         alignItems: "center", 
         justifyContent: 'flex-start',
         width: "98%",
         borderBottomWidth: 2,
         borderBottomColor: colors.dark_purple,
+        height:75,
     },
 
     text: {
