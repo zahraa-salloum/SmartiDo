98daa53479bec5974a268740a64f838c4973504f
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-29 15:35:01 +0300

    Frontend: Created profile component

---
+++ SmartiDo-mobile/src/components/Profile/index.tsx
@@ -0,0 +1,15 @@
+import { Image, ImageSourcePropType, Text, View } from 'react-native'
+import React, { FC } from 'react'
+
+
+interface ProfileProps  {
+}
+
+const Profile: FC<ProfileProps> = (props) => {
+    return (
+        <View style={styles.container}>
+        </View>
+    )
+}
+
+export default Profile
