a25d3c00b28a5161f3c4ce1c23b48078b5d2f2bc
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 16:44:52 +0300

    Frontend: Added image background and safe area view for times screen

--- SmartiDo-mobile/src/screens/Times/index.tsx
+++ SmartiDo-mobile/src/screens/Times/index.tsx
@@ -1,11 +1,16 @@
 import React, { FC, useEffect, useState } from 'react';
-
+import { ImageBackground, SafeAreaView } from 'react-native';
+import styles from './styles';
 
 interface TimesScreenProps  {}
 
 const TimesScreen: FC<TimesScreenProps> = (props) => {
 
     return (
+        <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
+            <SafeAreaView style={styles.container}>
+            </SafeAreaView>
+        </ImageBackground>
     )
 }
 
--- SmartiDo-mobile/src/screens/Times/styles.ts
+++ SmartiDo-mobile/src/screens/Times/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { colors } from '../../constants/constants';
-import styles from './styles';
+
 
 export default StyleSheet.create({
     containerBackground: {
