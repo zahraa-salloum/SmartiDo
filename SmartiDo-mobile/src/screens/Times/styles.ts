f6d7a27cd55b0238e825a63da4e5a9d915d933cc
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 16:43:20 +0300

    Frontend: Created styles file for times screen

---
+++ SmartiDo-mobile/src/screens/Times/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../constants/constants';
+import styles from './styles';
+
+export default StyleSheet.create({
+    containerBackground: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      width: "90%",
+      marginTop: 30,
+    },
+})
