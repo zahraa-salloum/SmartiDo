1f73162898e34c7a05ca01f3f13fc336bc467e0d
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 16:42:14 +0300

    Frontend: Created times screen

---
+++ SmartiDo-mobile/src/screens/Times/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useEffect, useState } from 'react';
+
+
+interface TimesScreenProps  {}
+
+const TimesScreen: FC<TimesScreenProps> = (props) => {
+
+    return (
+    )
+}
+
+export default TimesScreen
