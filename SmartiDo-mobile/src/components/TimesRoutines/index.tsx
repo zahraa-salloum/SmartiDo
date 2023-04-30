92c0865b180e594ada6425e786553335c117e68f
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 17:06:56 +0300

    Frontend: Created TimesRoutines component

---
+++ SmartiDo-mobile/src/components/TimesRoutines/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+import styles from './styles'
+
+
+interface TimesRoutinesProps  {
+}
+
+const TimesRoutines: FC<TimesRoutinesProps> = (props) => {
+    return (
+    <>
+</>
+    )
+}
+
+export default TimesRoutines
