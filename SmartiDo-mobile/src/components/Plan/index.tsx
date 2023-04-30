c338715378827ef1970a72a7ee32a1212b1d6409
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 11:08:34 +0300

    Frontend: Created Plan componenet

---
+++ SmartiDo-mobile/src/components/Plan/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React, { FC } from 'react'
+
+
+interface PlanProps  {
+}
+
+const Plan: FC<PlanProps> = (props) => {
+    return (
+        <View style={styles.container}>
+        </View>
+    ) 
+}
+
+export default Plan
