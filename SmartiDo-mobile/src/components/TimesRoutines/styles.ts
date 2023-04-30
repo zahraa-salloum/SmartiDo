b2781dcffd2405faa7076d9584ee5be202fd59e2
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-04-30 17:07:58 +0300

    Frontend: Created styles file for TimesRoutines component

---
+++ SmartiDo-mobile/src/components/TimesRoutines/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+import { colors } from '../../constants/constants'
+
+export default StyleSheet.create({
+    container: {
+        flex: 0.12,
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "center",
+        width: "98%",
+        borderColor: colors.dark_purple,
+        borderWidth: 2,
+        height:75,
+    },
+})
