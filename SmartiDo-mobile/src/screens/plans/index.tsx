077ba7e60533dd31aad9729384d1a176cdcd9712
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-01 17:31:32 +0300

    Frontend: Implemented get plan for the today api

--- SmartiDo-mobile/src/screens/plans/index.tsx
+++ SmartiDo-mobile/src/screens/plans/index.tsx
@@ -1,22 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './styles';
-import { ImageBackground, SafeAreaView } from 'react-native';
+import { ImageBackground, SafeAreaView, ScrollView, Text } from 'react-native';
 import EmptyState from '../../components/EmptyState';
 import RoundButton from '../../components/RoundButton';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import { numbers } from '../../constants/constants';
+import * as SecureStore from 'expo-secure-store';
+import Plan from '../../components/Plan';
 
 interface PlansScreenProps  {}
 
 const PlansScreen: FC<PlansScreenProps> = (props) => {
+    const [plan, setPlan] = useState([]);
+    const [visible, setVisible] = React.useState(false);
     const navigation = useNavigation();
+
+    const today = new Date();
+    useEffect(() => {
+        const fetchPlan  = async () => {
+            const token = await SecureStore.getItemAsync('token');
+            
+            const data = {
+                day: today.toISOString().substring(0, 10),
+            };
+            axios.post("http://"+ numbers.server +"/api/v0.0.1/get_plan", data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then(response => {
+                setPlan(response.data.plan);
+            })
+        }   
+        fetchPlan();
+    }, []);
+
 return (
     <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
         <SafeAreaView style={styles.container}>
-            <EmptyState image={require('../../../assets/plans.png')} title='No Plan Yet' description='Tap + to create' />
+            <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent}>
+                <Text style={styles.title}>{today.toISOString().substring(0, 10)}</Text>
+                {plan.map((item) => (
+                    <Plan hour={item.hour} plan={item.task} key={item.id} />
+                ))}
+            </ScrollView>
+            {/* <EmptyState image={require('../../../assets/plans.png')} title='No Plan Yet' description='Tap + to create' />
             <RoundButton buttonprops={{
             title: "+",
             onPress: () => navigation.navigate("Times"),
-            }} />
+            }} /> */}
         </SafeAreaView>
     </ImageBackground>
 )
--- SmartiDo-mobile/src/screens/plans/styles.ts
+++ SmartiDo-mobile/src/screens/plans/styles.ts
@@ -12,5 +12,21 @@ export default StyleSheet.create({
       alignItems: "center",
       justifyContent: "center",
       width: "90%",
-    }
+      marginTop: 30,
+    },
+    scrollView:{
+      flex:1,
+      width: "100%",
+  },
+  scrollViewContent:{
+      paddingBottom: 30,
+      gap:10,
+  },
+  title:{
+    color: colors.purple, 
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    },
 })
