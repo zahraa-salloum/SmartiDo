eeaa355fe38d837cc03a372788f3698cab5ad1bf
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-01 18:05:38 +0300

    Frontend: Updated leaderboard screen to use useFocusEffect

--- SmartiDo-mobile/src/screens/leaderboard/index.tsx
+++ SmartiDo-mobile/src/screens/leaderboard/index.tsx
@@ -1,36 +1,35 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import { FlatList, ImageBackground, SafeAreaView, View } from 'react-native';
 import styles from './styles';
 import Record from '../../components/Record';
 import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
 import { numbers } from '../../constants/constants';
+import { useFocusEffect } from '@react-navigation/native';
 
 interface LeaderboardScreenProps  {}
 
 const LeaderboardScreen: FC<LeaderboardScreenProps> = (props) => {
     const [records, setRecords] = useState([]);
 
-    useEffect(() => {
-        
-        const fetchRecords  = async () => {
-            const token = await SecureStore.getItemAsync('token');
-            axios.get("http://"+ numbers.server +"/api/v0.0.1/get_records", {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-                }).then(response => {
-                    setRecords(response.data.records)
-                })
-            }
-            fetchRecords();
-
-            const intervalId = setInterval(() => {
-                fetchRecords();
-            }, 100000);
     
-            return () => clearInterval(intervalId);
-      }, []);
+    const fetchRecords  = async () => {
+        const token = await SecureStore.getItemAsync('token');
+        axios.get("http://"+ numbers.server +"/api/v0.0.1/get_records", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+                setRecords(response.data.records)
+        })
+    }
+            
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchRecords();
+        }, [])
+    );
+            
     return (
         <ImageBackground source={require('../../../assets/empty.png')} style={styles.containerBackground}>
             <SafeAreaView style={styles.container}>
