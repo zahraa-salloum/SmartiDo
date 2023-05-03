8106d1e30a3a86e4ac014f6f32c9ad408a1e0837
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 23:48:08 +0300

    Frontend: Provided navigation to statistics page

--- smartido-website/src/App.js
+++ smartido-website/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from './Pages/LoginPage';
 import UsersPage from "./Pages/UsersPage";
+import StatisticsPage from "./Pages/StatisticsPage";
 
 function App() {
     return (
       <Routes>
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/users" element={<UsersPage/>}/>
+          <Route path="/statistics" element={<StatisticsPage/>}/>
       </Routes> 
     );
 }
