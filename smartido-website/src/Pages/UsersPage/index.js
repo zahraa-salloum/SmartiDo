f768aeed1fa3d5fdab6c4670821b98eeb53b5915
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-04 01:11:16 +0300

    Frontend: Modified style for users container

--- smartido-website/src/Pages/UsersPage/index.js
+++ smartido-website/src/Pages/UsersPage/index.js
@@ -7,7 +7,9 @@ const UsersPage = () => {
     return (
         <>
         <Navbar />
-        <div className="container_users">
+        <div className="container_all_users">
+            <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
+            <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
             <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
         </div>
         </>
