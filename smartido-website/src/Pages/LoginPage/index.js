639eec0addfc3bc8d7eec6ccb53e9657aed49f74
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 18:05:59 +0300

    Frontend: Implemented log in for admin

--- smartido-website/src/Pages/LoginPage/index.js
+++ smartido-website/src/Pages/LoginPage/index.js
@@ -2,6 +2,7 @@ import Button from '../../components/Button';
 import Input from "../../components/Input";
 import { useNavigate } from "react-router-dom";
 import {useState } from "react";
+import axios from "axios";
 
 const Login = () => {
     const navigator = useNavigate();
@@ -12,8 +13,30 @@ const Login = () => {
     const handleEmail=(e)=>{
         setEmail(e.target.value)
     }
-   const handlePassword=(e)=>{
+
+    const handlePassword=(e)=>{
        setPassword(e.target.value)
+    }
+
+    const handleSubmit=()=>{
+    
+        let data = {
+            "email": email,
+            "password": password
+          };
+
+        axios.post("http://192.168.1.104:8000/api/v0.0.1/auth/login",data).then((res) => {
+            if(res.data.user.type_id == 1){
+                localStorage.setItem('token',res.data.authorisation.token);
+                // navigator('/welcome');
+            } else {
+                setError("Access Denied")
+            }
+        }
+        ).catch((err) => {
+            console.log(err);
+        })
+
     }
 
     return (
@@ -21,8 +44,8 @@ const Login = () => {
           <h1 className="size_heading">Log In</h1>
           <Input label_name={"Email"} input_type={"email"} onChange={handleEmail} size="40" />
           <Input label_name={"Password"} input_type={"password"} onChange={handlePassword}/>
-          <Button name_button={"LOGIN"} />
-          <div className="error"></div>
+          <Button name_button={"LOGIN"} onSubmit={handleSubmit}/>
+          <div className="error">{error}</div>
         </div>
        
     );
