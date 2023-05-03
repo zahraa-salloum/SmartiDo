0ffdafd82dd95f5cdfe6414728b9511ea23c533b
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-04 00:40:30 +0300

    Frontend: Implemented User component

--- smartido-website/src/components/User/index.js
+++ smartido-website/src/components/User/index.js
@@ -2,6 +2,12 @@
 
 const User=({user_name,user_image,onBlock})=>{  
     return(
+        <div className="user_container">
+            <img src={user_image} className="user_image" />
+            <div className="user_name"> {user_name} </div>
+            <button className='user_block_btn' onClick={onBlock}>
+            </button>
+        </div>
     );
 }
 
