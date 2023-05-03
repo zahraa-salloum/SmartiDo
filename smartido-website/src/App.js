e6820ec0dd31808981fb95b4b42d7c3df706f41f
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 15:53:41 +0300

    Frontend: Added background img to login page

--- smartido-website/src/App.js
+++ smartido-website/src/App.js
@@ -1,9 +1,12 @@
-
+import { Routes, Route } from "react-router-dom";
+import LoginPage from './Pages/LoginPage';
 
 function App() {
-  return (
-    
-  );
+    return (
+      <Routes>
+          <Route path="/login" element={<LoginPage/>}/>
+      </Routes> 
+    );
 }
 
 export default App;
--- smartido-website/src/Pages/LoginPage/index.js
+++ smartido-website/src/Pages/LoginPage/index.js
@@ -1,10 +1,10 @@
-
+import './styles.css';
 
 
 const Login = () => {
     return (
-        <>
-        </>
+        <div class="login-page">
+       </div>
     );
 }
 export default Login;
