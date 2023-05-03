ef61edea4b220ad61539e23e3929d0183fe7aeb6
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 23:44:35 +0300

    Frontend: Created statistics page

---
+++ smartido-website/src/Pages/StatisticsPage/index.js
@@ -0,0 +1,14 @@
+import Navbar from "../../components/Navbar";
+
+
+
+const StatisticsPage = () => {
+    return (
+        <>
+        <Navbar />
+        </>
+    );
+}
+
+
+export default StatisticsPage;
