7aaad578535b97f43ede07bae9357091345a7f96
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 16:28:15 +0300

    Frontend: Added button to login page

--- smartido-website/src/Pages/LoginPage/index.js
+++ smartido-website/src/Pages/LoginPage/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from "../../components/Input";
 
 const Login = () => {
@@ -7,6 +8,7 @@ const Login = () => {
           <h1 className="size_heading">Log In</h1>
           <Input label_name={"Email"} input_type={"email"} size="40" />
           <Input label_name={"password"} input_type={"password"} />
+          <Button name_button={"Login"} />
           </div>
        
     );
