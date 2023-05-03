7f21582216a3b16130299e10af2d6f2de42a98ec
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 23:32:28 +0300

    Frontend: Created users page

---
+++ smartido-website/src/Pages/UsersPage/index.js
@@ -0,0 +1,12 @@
+
+
+
+const UsersPage = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+
+export default UsersPage;
--- smartido-website/src/components/Navbar/SidebarData.js
+++ smartido-website/src/components/Navbar/SidebarData.js
@@ -3,12 +3,12 @@ import * as AiIcons from 'react-icons/ai';
 
 
 export const SidebarData = [
-  {
-    title: 'Home',
-    path: '/',
-    icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text'
-  },
+  // {
+  //   title: 'Home',
+  //   path: '/',
+  //   icon: <AiIcons.AiFillHome />,
+  //   cName: 'nav-text'
+  // },
   {
     title: 'Users',
     path: '/users',
