b10c4e54ef740cb7375c3479421d9b30b5b54727
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-04 01:26:16 +0300

    Frontend: Implemented get users api

--- smartido-website/src/Pages/UsersPage/index.js
+++ smartido-website/src/Pages/UsersPage/index.js
@@ -1,16 +1,36 @@
 import Navbar from "../../components/Navbar";
 import User from "../../components/User";
-
+import {useState, useEffect } from "react";
+import axios from "axios";
+import { numbers } from "../../constants/constants";
 
 
 const UsersPage = () => {
+    const [users, setUsers] = useState([]);
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        const getUsers = () => {
+            axios.get("http://"+ numbers.server +"/api/v0.0.1/admin/get_users", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+                }).then(response => {
+                    setUsers(response.data.users)
+                }).catch((err) => {
+                    console.log(err);
+                })
+        }
+        getUsers()
+    },[]) 
+
     return (
         <>
         <Navbar />
         <div className="container_all_users">
-            <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
-            <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
-            <User user_image={require("../../assets/logo.png")} user_name={'Zahraa'} />
+            {users.map(user => (
+                <User key={user.id} user_image={`data:image/png;base64,${user.picture}`} user_name={user.name} />
+            ))}
         </div>
         </>
     );
