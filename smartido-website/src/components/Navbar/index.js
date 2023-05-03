40805c330dc09cb05922c2063cf3e80d0b40b0c8
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 23:28:31 +0300

    Frontend: Added logo image to the navbar

--- smartido-website/src/components/Navbar/index.js
+++ smartido-website/src/components/Navbar/index.js
@@ -20,6 +20,7 @@ function Navbar() {
             </div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
+                    <img src={require("../../assets/logo.png")} className='logo_navbar'/>
                     <li className='navbar-toggle'>
                         <Link to='#' className='menu-bars'>
                             <AiIcons.AiOutlineClose />
