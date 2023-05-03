c77d328e806fe3c7b0b751b872fffc3449adbcf2
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 15:56:57 +0300

    Frontend: Added button to button component

--- smartido-website/src/components/Button/index.js
+++ smartido-website/src/components/Button/index.js
@@ -3,6 +3,9 @@
 
 const Button = ({name_button,onSubmit}) => {
     return (
-);
+        <button className='btn' onClick={onSubmit}>
+            {name_button}
+        </button>
+    );
 }
 export default Button;
