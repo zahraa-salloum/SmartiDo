cad29918ea160bc4bef594d356127caac2b10b5d
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 16:51:07 +0300

    Frontend: Added style for input_white

--- smartido-website/src/components/Input/index.js
+++ smartido-website/src/components/Input/index.js
@@ -5,7 +5,7 @@ const Input=({label_name,input_type,onChange},)=>{
     return(
     <div className="container_clear">
         <label className="label">{label_name}</label>
-        <input className="input_clear" type={input_type} onChange={onChange} required/>
+        <input className="input_white" type={input_type} onChange={onChange} required/>
     </div>
     );
 }
