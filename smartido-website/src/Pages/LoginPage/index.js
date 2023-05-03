6c37e59f53ebcae0c777bd61043008c1f041e8c3
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-03 16:58:20 +0300

    Frontend: Changed labels

--- smartido-website/src/Pages/LoginPage/index.js
+++ smartido-website/src/Pages/LoginPage/index.js
@@ -7,8 +7,8 @@ const Login = () => {
         <div className="container">
           <h1 className="size_heading">Log In</h1>
           <Input label_name={"Email"} input_type={"email"} size="40" />
-          <Input label_name={"password"} input_type={"password"} />
-          <Button name_button={"Login"} />
+          <Input label_name={"Password"} input_type={"password"} />
+          <Button name_button={"LOGIN"} />
           <div className="error"></div>
         </div>
        
