3cdd3da2bf16335c32ee119e3e62bbc7989868c3
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-04 03:26:09 +0300

    Frontend: Added style for statistic component

--- smartido-website/src/components/Statistic/index.js
+++ smartido-website/src/components/Statistic/index.js
@@ -2,7 +2,7 @@
 
 const Statistic=({statistic_title,statistic_number})=>{
     return(
-        <div>
+        <div className="statistic_container">
             <div className="statistic_title">{statistic_title}</div>
             <div className="statistic_number">{statistic_number}</div>
         </div>
