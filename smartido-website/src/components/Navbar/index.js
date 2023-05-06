3a77b63777a2c9ef130c795b789d02537750d7cd
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-05-06 15:49:45 +0300

    Frontend: Set side nav static

--- smartido-website/src/components/Navbar/index.js
+++ smartido-website/src/components/Navbar/index.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { SidebarData } from './SidebarData';
-import { IconContext } from 'react-icons';
+
 
 const Navbar = ({name}) => {
     const [sidebar, setSidebar] = useState(false);
@@ -16,22 +14,14 @@ const Navbar = ({name}) => {
 
     return (
     
-        <IconContext.Provider value={{ color: '#fff' }}>
+        <>
             <div className='navbar'>
-                <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars onClick={() => setSidebar((prev) => !prev)} />
-                </Link>
                 <div className='hello-message'>Hello... {name}</div>
                 <button className='logout_btn' onClick={logout}>LOGOUT</button>
             </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={() => setSidebar((prev) => !prev)}>
+            <nav className='nav-menu active'>
+                <ul className='nav-menu-items'>
                     <img src={require("../../assets/logo-no-background white.png")} className='logo_navbar'/>
-                    <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            <AiIcons.AiOutlineClose />
-                        </Link>
-                    </li>
                     {SidebarData.map((item, index) => {
                         return (
                             <li key={index} className={item.cName}>
@@ -44,9 +34,9 @@ const Navbar = ({name}) => {
                     })}
                 </ul>
             </nav>
-        </IconContext.Provider>
+        </>
     
-  );
+    );
 }
 
 export default Navbar;
